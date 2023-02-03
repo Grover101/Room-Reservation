6b5201bb37d8e9a412e2cf66a57149a7e5654ab9
Grover101 <limachigrover09@gmail.com>
2023-02-03 15:31:00 -0400

    add routers

--- app/app.js
+++ app/app.js
@@ -9,16 +9,18 @@ app.use(express.json({ limit: '50mb' }))
 app.use(cors())
 
 app.get('/api/v1', (_req, res) => {
-    return res.json({ message: 'Hello World!!!asdfds' })
+    return res.json({ message: 'Hello World!!!' })
 })
 
 sequelize
-    .authenticate()
+    .sync()
     .then(() => {
         console.log('DB Connection Local')
     })
     .catch(error => {
         console.log('Failed to Connection DB Local\n', error)
     })
+
+app.use('/api/v1', require('./routes/router'))
 
 module.exports = app
