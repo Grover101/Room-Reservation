8811f99a84add0a2f7c0d29ce4266080e91ce861
Grover101 <limachigrover09@gmail.com>
2023-02-04 16:39:30 -0400

    add help validates

--- app/helpers/db-validates.js
+++ app/helpers/db-validates.js
@@ -1,13 +1,52 @@
-const { sequelize } = require('../models/index')
+const { Op } = require('sequelize')
+const { sequelize, Room } = require('../models/index')
 
-const attributeExists = async (value = '', key = '', table = '') => {
+const attributeExists = async (
+    value = '',
+    key = '',
+    table = '',
+    messageError = ''
+) => {
     const query = await sequelize.query(
         `SELECT * FROM ${table} WHERE ${key} = '${value}' LIMIT 1`,
         { type: sequelize.QueryTypes.SELECT }
     )
-    if (query.length > 0) throw new Error(`${key}: ${value}, already exists`)
+    if (query.length > 0)
+        throw new Error(
+            messageError.length
+                ? messageError
+                : `${key}: ${value}, already exists`
+        )
+}
+
+const alreadyReserved = async (id = '') => {
+    const query = await Room.findOne({
+        where: {
+            [Op.and]: [
+                { id },
+                { [Op.or]: [{ state: 'Reserved' }, { state: 'Mantenance' }] }
+            ]
+        }
+    })
+    if (query) throw new Error(`Room ID ${id} already reserved`)
+}
+
+const Exists = async (value = '', key = '', table = '') => {
+    const query = await sequelize.query(
+        `SELECT * FROM ${table} WHERE ${key} = '${value}' LIMIT 1`,
+        { type: sequelize.QueryTypes.SELECT }
+    )
+    if (query.length <= 0) throw new Error(`${key}: ${value}, not exists`)
+}
+
+const dateValidated = entryDate => {
+    const dateInit = new Date(entryDate)
+    if (!(dateInit > new Date())) throw new Error(`Entry date cannot be passed`)
 }
 
 module.exports = {
-    attributeExists
+    attributeExists,
+    Exists,
+    alreadyReserved,
+    dateValidated
 }
