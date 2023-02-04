c428b1ffa9e39dade0dfe82b677fcae122e0c611
Grover101 <limachigrover09@gmail.com>
2023-02-04 16:40:12 -0400

    add config for upload images

--- app/app.js
+++ app/app.js
@@ -4,9 +4,24 @@ const express = require('express')
 const app = express()
 const { sequelize } = require('./models/index')
 const cors = require('cors')
+const fileUpload = require('express-fileupload')
 
-app.use(express.json({ limit: '50mb' }))
+app.use(express.json({ limit: '10mb' }))
+app.use(
+    express.urlencoded({
+        extended: false,
+        limit: '10mb',
+        parameterLimit: 500
+    })
+)
 app.use(cors())
+app.use(
+    fileUpload({
+        useTempFiles: true,
+        tempFileDir: '/tmp/',
+        createParentPath: true
+    })
+)
 
 app.get('/api/v1', (_req, res) => {
     return res.json({ message: 'Hello World!!!' })
